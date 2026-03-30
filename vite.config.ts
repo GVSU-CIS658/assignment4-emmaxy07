cb559a845ef78e4331c7856fd666d9da5cb01574
Emmanuel Omole <emmanueliyare7@gmail.com>
2026-03-30 10:11:33 -0400

    new push

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "assignment4-emmaxy07",
   css: {
     preprocessorOptions: {
       scss: {
